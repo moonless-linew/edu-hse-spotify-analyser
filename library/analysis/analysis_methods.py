3d80902a7377ec85f7424bd04c7ec96f2a427ead
Arseniy Sablin <amsablin@edu.hse.ru>
2023-05-05 20:18:45 +0300

    edit docstrings to methods in analysis_methods.py

--- library/analysis/analysis_methods.py
+++ library/analysis/analysis_methods.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 
 
 def average(param_key, param_number, df, color, title):
-    '''
-    Функция для получения зависимости
+    """
+    Функция для построения графика зависимости
     среднего численного параметра от качественного параметра
-    Возвращает лист ключей и лист значений
-    '''
+    """
     # param_key - качественное значение по типу key, mode
     # param_number - количественное значение по типу liveness, popularity
 
@@ -22,17 +21,17 @@ def average(param_key, param_number, df, color, title):
 
 
 def describe_artist(name, param, df):
-    '''
-    Функция для описания параметра артиста
+    """
+    Функция для описания параметра артиста.
     Возвращает словарь
-    '''
+    """
     return df.loc[df["artist_name"] == name, param].describe().to_dict()
 
 
 def corr_matrix(df):
-    '''
-    Функция для построения матрицы корреляции
-    '''
+    """
+    Функция для построения матрицы корреляций
+    """
     f = plt.figure(figsize=(15, 8))
     plt.matshow(df.corr(), fignum=f.number)
 
@@ -54,10 +53,10 @@ def corr_matrix(df):
 
 
 def top_tracks(key, count, df):
-    '''
-    Функция для получения топа треков по параметру
+    """
+    Функция для получения топа треков по параметру.
     Возвращает лист названия столбцов и лист листов значений
-    '''
+    """
     df_sorted = df.sort_values([key, "artist_name", "track_name"], ascending=[False, True, True]).head(count)
     df_sorted = df_sorted[["artist_name", "track_name", "genre", key]]
     out = []
@@ -72,10 +71,10 @@ def top_tracks(key, count, df):
 
 
 def artist_popularity(name, df, color):
-    '''
-    Функция для получения популярности треков артиста по порядку
-    Возвращает лист индексов и лист значений популярности
-    '''
+    """
+    Функция для построения графика
+    популярности треков артиста по порядку
+    """
     df2 = df.loc[df["artist_name"] == name]["popularity"]
     y = df2.values.tolist()
     x = [k for k in range(len(y))]
@@ -88,6 +87,10 @@ def artist_popularity(name, df, color):
 
 
 def genre_evolution(genre, df, color):
+    """
+    Функция для построения графика
+    популярности треков жанра по порядку
+    """
     df2 = df.loc[df["genre"] == genre]["popularity"]
     y = df2.values.tolist()
     x = [k for k in range(len(y))]
